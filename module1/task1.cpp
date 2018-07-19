d2c4f111e4cb9fc1ef81d2fa94f5624eb18c38df
Andrey Larin <larin.projects@gmail.com>
2018-07-19 16:17:09 +0300

    Added all tasks from module1

---
+++ module1/task1.cpp
@@ -0,0 +1,27 @@
+// Определите только функцию power, где
+// x - число, которое нужно возвести в степень
+// p - степень, в которую нужно возвести x
+
+#include <iostream>
+
+using namespace std;
+
+int power(int x, unsigned p) {
+    int answer = 1;
+    int i;
+    for (i = p; i > 0; i--) {
+        answer *= x;
+    }
+    return answer;
+}
+
+int main() {
+    int x = 0;
+    unsigned p = 0;
+
+    cout << "Enter 2 integers:\n";
+    cin >> x >> p;
+    cout << power(x, p) << endl;
+
+    return 0;
+}
